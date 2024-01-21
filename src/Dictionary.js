7f7eb3d3784e8fc77377d637a5f0160edf684460
taegen-seto <taegenseto@gmail.com>
2024-01-22 09:42:21 +1000

    install axios and added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    // documentation: https://www.shecodes.io/learn/apis/dictionary
+    let apiKey = "384310c338bfe3o7f06caedc66df497t";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
